1b2e1e2f3457470ad099e83521ebb6b52960adeb
xiao <1280765063@qq.com>
2021-12-19 19:56:15 +0800

    master commit2

--- src/main/java/com/x/test.java
+++ src/main/java/com/x/test.java
@@ -6,6 +6,9 @@ public class test {
         System.out.println("hello world2");
         System.out.println("hello world3");
         System.out.println("hello world4");
+        System.out.println("master test");
+
+
 
     }
 }
